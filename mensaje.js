6e728f27bc6bff5683cacd38c725e5ee7bb8568b
Julinho Federico Romero Sobrado <86712716+sam1572125750@users.noreply.github.com>
2024-02-13 16:59:06 -0500

    versión final de la barbería

---
+++ mensaje.js
@@ -0,0 +1,21 @@
+function compra1(){
+    let cantidad=parseInt(prompt("ingrese la cantidad  a comprar"))
+    let total=10*cantidad;
+   alert("El costo total es de $"+total) 
+   alert("vuelva pronto ")  
+}
+function compra2(){
+    let cantidad=parseInt(prompt("ingrese la cantidad  a comprar"))
+    let total=8*cantidad;
+   alert("El costo total es de $"+total)   
+}
+function compra3(){
+    let cantidad=parseInt(prompt("ingrese la cantidad  a comprar"))
+    let total=15*cantidad;
+   alert("El costo total es de $"+total) 
+
+}
+/*esto es para el formulario */
+function compras10(){
+   alert("Reserva exitosa") 
+}
